721d79a36bfd7ea9834408587a78e43e683797b6
126993 <jothamonyango45@gmail.com>
2023-08-11 05:15:53 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - a program that prints a line using printf function
  *
- * Return: always 0
+ * Return: 0 when successful
  */
 
 int main(0)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
