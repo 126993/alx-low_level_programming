d059c1c0eda52a7614445679ba538aa3a972f37f
126993 <jothamonyango45@gmail.com>
2023-08-11 04:34:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line using puts funtion
- *return: 0 (Victory)
+ * main - a program that prints a line using puts funtion
+ * Return: 0
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints a line using printf function
+ *
+ * Return: always 0
+ */
+
+int main(0)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
