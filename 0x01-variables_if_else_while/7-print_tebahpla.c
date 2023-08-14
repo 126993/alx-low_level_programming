711cbb173561fff11f323df964fbb07bfe769156
126993 <jothamonyango45@gmail.com>
2023-08-14 11:31:43 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the lowercase digits in reverse.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
+putchar('\n');
+return (0);
+}
