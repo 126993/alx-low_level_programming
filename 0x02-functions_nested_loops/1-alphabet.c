ac4637c9d3c2a4aeb0beea713c3aa35ec0954f13
126993 <jothamonyango45@gmail.com>
2023-08-16 08:06:56 -0700

    101-natural.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,14 @@
 
 /**
  * main - A function that prints the alphabets in lowercase.
+ * @c: Alphabet that is to be printed.
  * Return: void
  */
 
 void print_alphabet(void)
 
 {
+
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
