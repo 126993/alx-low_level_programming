dc0aeada0cfb75c7f70391c5bb3fe62359998a61
126993 <jothamonyango45@gmail.com>
2023-08-18 03:53:10 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,18 +8,24 @@
 
 void print_triangle(int size)
 {
-	int size;
+	int row, hashes, spaces;
 
 	if (size <= 0)
-		putchar('\n');
 	{
-		int k;
-		int b;
-
-		for (k = 0 ; k <= size ; k++)
+		putchar('\n');
+	}
+	else
+	{
+		for (row = 1 ; row <= size ; row++)
 		{
-			for (b = 0 ; b <= size ; b++)
+			for (spaces = size - row ; spaces >= 1 ; spaces--)
+			{
+				putchar(' ');
+			}
+			for (hashes = 1 ; hashes <= row ; hashes++)
+			{
 				putchar('#');
+			}
 			putchar('\n');
 		}
 	}
